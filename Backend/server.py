ec57edde30eb73962117e9dcc4849622dbce9463
Durval <pedrocruzeiro@ua.pt>
2023-03-13 19:38:34 +0000

    "update"

--- Backend/server.py
+++ Backend/server.py
@@ -2,50 +2,69 @@ from flask import Flask, send_from_directory, request, make_response
 from flask_cors import CORS
 from werkzeug.datastructures import FileStorage
 from pymongo import MongoClient
-import gridfs
+from gridfs import GridFS
 import json
 import os
-
-client = MongoClient("mongodb://localhost:27017")
-db = client['peci']
-collection = db.test_collection
-collection.insert_one({"test":"big string"})
-#fs = gridfs.GridFS(db)
-
+from Mongo_cli import MongoCli
 
 app = Flask(__name__)
+mongo_cli = MongoCli()
+fs = GridFS(mongo_cli.db)
 CORS(app)
+
+
+class Operations:
+    def __init__(self): 
+        pass
+
+    def upload(self):
+        file = request.files['file']    
+        description = json.loads(request.form['description'])
+        location = './' + description['name'] + '.webm'
+        file.save(location)
+        _id = mongo_cli.generate_unique_id()
+        mongo_cli.insert_data(location,_id,"location")
+        mongo_cli.insert_media_file(_id,location)
+        os.remove(location)
+        return "location"
+    
+    def list_videos(self):
+        ret = []
+        files = mongo_cli.collection.find()
+        for f in files:
+            ret.append(f['location'])
+        return ret
+    
+    def download(self):
+        fileName = request.get_json()['name']
+        name = mongo_cli.generate_from_db(mongo_cli.collection.find_one({"location": fileName})['_id'])
+        return name
+    
+operation = Operations()
 
 @app.route('/')
 def default():
-    
     return "Running..."
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    file = request.files['file']
-    description = json.loads(request.form['description'])
-    file.save('./'+description['name']+'.webm')
-    # a = fs.put(file)
-    return {'status': 200}
+    print("UPLOAD")
+    return operation.upload()
 
 @app.route('/download', methods=['POST'])
 def download():
-    fileName = request.get_json()['name']
-    response = make_response()
-    response.headers.add_header('Access-Control-Allow-Origin', '*')
-    response.content_type = 'video/webm'
-    with open("./"+fileName, "rb") as f:
-        response.data = f.read()
-    return response
+    print("DOWNLOAD")
+    return operation.download()
 
 @app.route('/list_videos')
 def list_videos():
-    return [i for i in os.listdir('.') if i.endswith('.webm')]
+    print("LIST")
+    return operation.list_videos()
 
 # audio 
 @app.route('/upload_audio', methods=['POST'])
 def upload_audio():
+    print("UPLOAD AUDIO")
     file = request.files['file']
     description = json.loads(request.form['description'])
     file.save('./'+description['name']+'.mp3')
@@ -54,6 +73,7 @@ def upload_audio():
 
 @app.route('/download_audio', methods=['POST'])
 def download_audio():
+    print("DOWNLOAD AUDIO")
     # download audio to Backend folder
     fileName = request.get_json()['name']
     response = make_response()
@@ -65,6 +85,7 @@ def download_audio():
 
 @app.route('/list_audio')
 def list_audio():
+    print("LIST AUDIO")
     return [i for i in os.listdir('.') if i.endswith('.mp3')]
 
 if __name__ == '__main__':
