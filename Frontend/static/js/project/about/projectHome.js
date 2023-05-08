a700583c250edf7ba12a19fd360e4016f1dabfc5
Durval <pedrocruzeiro@ua.pt>
2023-05-08 20:31:45 +0100

    "export & import"

--- Frontend/static/js/project/about/projectHome.js
+++ Frontend/static/js/project/about/projectHome.js
@@ -46,5 +46,71 @@ const delete_project = async () => {
   let a = await response.json()
 
   alert("Project Deleted! You will be redirected to MyProjects page!");
-  window.location.href = '../../MyProjects'
+  window.location.href = '../../myProjects'
+}
+
+document.getElementById("folder-button").addEventListener("click", function () {
+  document.getElementById("folder-upload").click();
+});
+
+document.getElementById("folder-upload").addEventListener("change", function () {
+  var folderName = this.value.split("\\").pop();
+  document.getElementById("folder-name").innerHTML = folderName;
+});
+
+let folderInput = document.getElementById("folder-upload");
+let folderNameSpan = document.getElementById("folder-name");
+folderInput.setAttribute("webkitdirectory", ""); // Only accept video inputs
+folderInput.setAttribute("directory", "");
+
+folderInput.addEventListener("change", async () => {
+  
+  // get folder name
+  let folderName = folderInput.files[0].webkitRelativePath.split("/")[0];
+
+  // console.log("FOLDERRR")
+  folderNameSpan.innerHTML = "<b>Folder Name: </b>" + folderName;
+
+  // get all files from folder
+  let files = folderInput.files;
+
+  let blobs = [];
+  for (let i = 0; i < files.length; i++) {
+      let file = files[i];
+      id = file.name.split(".")[0]
+      blobs.push({blob: file, name: file.name, id: id})
+    }
+    storeFolder(blobs)
+})
+
+const storeFolder = async (blobs) => {
+  for (let blob of blobs) {
+      let data = new FormData();
+      data.append('file',blob.blob);
+
+      data.append('description', JSON.stringify({ name: blob.name, pid: projectID, vid: blob.id}))
+
+      $('#acquisitionVideoPreviewModal').modal('hide')
+
+      let response = await fetch('http://127.0.0.1:5001/upload_folder', {
+          method: "POST",
+          body: data
+      });
+  }
+  let data = new FormData()
+  data.append('description', JSON.stringify({pid: projectID}))
+  let response = await fetch('http://127.0.0.1:5001/update', {
+    method: "POST",
+    body: data
+  })
+}
+
+const export_project = async () => {
+  let data = new FormData()
+  data.append('description', JSON.stringify({pid: projectID}))
+
+  let response = await fetch('http://127.0.0.1:5001/export_project', {
+          method: "POST",
+          body: data
+      });
 }
