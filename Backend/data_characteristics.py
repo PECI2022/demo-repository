8c4cb4f25eb3d829efc6514be8195edce5c3c3dd
Tiago Mostardinha <tiago.mostardinha@ua.pt>
2023-05-09 23:21:42 +0100

    gets all characteristics from a video, working in requests to the server, to display the characteristics

--- Backend/data_characteristics.py
+++ Backend/data_characteristics.py
@@ -1,181 +1,150 @@
 import cv2
 import numpy as np
-import subprocess
 import json
 import os
-import math
 
 
-def getFileExtension(filname):
-    return os.path.splitext(filname)[1]
-
-
-def tomp4(video):
-    video_formats = [".mpeg", ".mpg", ".mpe", ".avi",
-                     ".wmv", ".flv", ".mov", ".webm", ".mkv"]
-    if getFileExtension(video) in video_formats:
-        print("Converting to mp4...")
-        new_video = video[:-len(getFileExtension(video))] + ".mp4"
-        subprocess.run(["ffmpeg", "-i", video, "-c:v", "libx264",
-                       "-preset", "slow", "-crf", "22", "-c:a", "copy", new_video])
-        return new_video
-
-
-def getContrast(video):
-    cap = cv2.VideoCapture(video)
-    rms_contrast = []
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-
-        # Convert the frame to grayscale
-        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-        # Calculate the RMS contrast of the frame
-        rms_contrast.append(frame_gray.std())
-
-    cap.release()
-    return np.mean(rms_contrast)
-
-# Na dúvida
-
-
-def getBrightness(video):
-    cap = cv2.VideoCapture(video)
-    avg_brightness = []
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-
-        # Calculate the mean brightness of the frame
-        brightness = cv2.mean(frame)[0]
-        avg_brightness.append(brightness)
-
-    cap.release()
-    return sum(avg_brightness) / len(avg_brightness)
-
-
-def getResolution(video):
-    command = ['ffprobe', '-v', 'error', '-select_streams', 'v:0',
-               '-show_entries', 'stream=width,height', '-of', 'csv=p=0', video]
-    output = subprocess.check_output(command).decode('utf-8').strip()
-    width, height = map(int, output.split(','))
-    return width, height
-
-
-def getDuration(video):
-    return float(subprocess.run(["ffprobe", "-i", video, "-show_entries", "format=duration", "-v", "quiet", "-of", "csv=p=0"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout)
-
-
-def getImageSharpness(img):
-    d_im = cv2.UMat(img)
-    d_lap = cv2.Laplacian(d_im, cv2.CV_32F)
-    h_lap = d_lap.get()
-    var = h_lap.var()
-    return var
-
-
-def getSharpness(video_path):
+def read_file(video_path):
     cap = cv2.VideoCapture(video_path)
-    sharpness = []
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        sharpness.append(getImageSharpness(gray))
-    cap.release()
-    return np.mean(sharpness)
 
-
-def getSaturation(video):
-    cap = cv2.VideoCapture(video)
-    avg_saturation = []
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-
-        # Convert the frame to HSV color space
-        frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-        # Extract the saturation channel from the HSV image
-        s_channel = cv2.split(frame_hsv)[1]
-
-        # Calculate the average saturation of the frame
-        avg_saturation.append(np.mean(s_channel))
-
-    cap.release()
-    return np.mean(avg_saturation)
-
-
-def getHue(video):
-    cap = cv2.VideoCapture(video)
-    hue_values = []
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-
-        # Convert the frame to HSV
-        frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-        # Extract the hue channel
-        hue = frame_hsv[:, :, 0]
-
-        # Compute the average hue of the frame
-        hue_values.append(np.mean(hue))
-
-    cap.release()
-    return np.mean(hue_values)
-
-
-def separeteAudio(video):
-    subprocess.run(['ffmpeg', '-i', video, '-vn',
-                   '-acodec', 'mp3', video[:-4] + ".mp3"])
-
-
-def getVideoCharacteristics(video):
-    if getFileExtension(video) != ".mp4":
-        return getVideoCharacteristics(tomp4(video))       # send to mp4 to be uniform
-    else:
-        contrast = float(getContrast(video))
-        brightness = float(getBrightness(video))
-        sharpness = float(getSharpness(video))
-        saturation = float(getSaturation(video))
-        hue = float(getHue(video))
-        resolution = list(getResolution(video))
-        duration = float(getDuration(video))
-
-        video_chars = {
-            "contrast": contrast,
-            "brightness": brightness,
-            "sharpness": sharpness,
-            "saturation": saturation,
-            "hue": hue,
-            "resolution": resolution,
-            "duration": duration
-        }
-        return json.dumps(video_chars)
-
-
-def main():
-    file = "./asdf.webm"
-    # print(getFileExtension(file))
-    # print(tomp4(file))
-    # print("Contrast", getContrast(tomp4(file)))
-    # print("Brightness", getBrightness(tomp4(file)))
-    # print(getVideoCharacteristics(tomp4(file)))
-    # print(getResolution(tomp4(file)))
-    # print(getDuration(tomp4(file)))
-    # print(getSharpness(tomp4(file)))
-    # print(getSaturation(tomp4(file)))
-    # print(getHue(tomp4(file)))
-    print(getVideoCharacteristics(file))
+    if not cap.isOpened():
+        print("Error: Unable to open video file.")
+        return
     
+    return cap
+
+def get_contrast(info):
+    cap = read_file(info)
+
+    total_contrast = 0
+    frame_count = 0
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        contrast = np.std(gray_frame)
+
+        total_contrast += contrast
+        frame_count += 1
+
+    cap.release()
+    return total_contrast / frame_count
+
+def get_brightness(info):
+    cap = read_file(info)
+
+    total_brightness = 0
+    frame_count = 0
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        average_brightness = cv2.mean(gray_frame)[0]
+        total_brightness += average_brightness
+        frame_count += 1
+    
+    cap.release()
+    return total_brightness / frame_count
 
 
-if __name__ == "__main__":
-    main()
+def compute_sharpness(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    laplacian = cv2.Laplacian(gray, cv2.CV_64F)
+    sharpness = np.var(laplacian)
+    return sharpness
+
+
+
+def get_sharpness(info):
+    cap = read_file(info)
+
+    sharpness_scores = []
+
+    while cap.isOpened():
+        success, frame = cap.read()
+        if success:
+            sharpness_score = compute_sharpness(frame)
+            sharpness_scores.append(sharpness_score)
+        else:
+            break
+
+    cap.release()
+
+    return np.mean(sharpness_scores)
+
+
+def get_saturation(info):
+    cap = read_file(info)
+
+    total_saturation = 0
+    frame_counter = 0
+    
+    while cap.isOpened():
+        ret, frame = cap.read()
+        
+        if not ret:
+            break
+        
+        hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        
+        # Calculate the average saturation of the frame
+        avg_saturation = np.mean(hsv_frame[:, :, 1])
+        
+        # Accumulate the saturation value
+        total_saturation += avg_saturation
+        
+        frame_counter += 1
+    
+    cap.release()
+    
+    return total_saturation / frame_counter
+
+
+def get_hue(info):
+    cap = read_file(info)
+    total_hue = 0
+    frame_counter = 0
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+
+        # Convert the frame from BGR to HSV color space
+        hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        # Calculate the average hue of the frame
+        average_hue = np.mean(hsv_frame[:, :, 0])
+
+        total_hue += average_hue
+        frame_counter += 1
+
+    return total_hue / frame_counter
+
+
+
+
+
+# def getResolution(video):
+#     command = ['ffprobe', '-v', 'error', '-select_streams', 'v:0',
+#                '-show_entries', 'stream=width,height', '-of', 'csv=p=0', video]
+#     output = subprocess.check_output(command).decode('utf-8').strip()
+#     width, height = map(int, output.split(','))
+#     return width, height
+
+
+# def getDuration(video):
+#     return float(subprocess.run(["ffprobe", "-i", video, "-show_entries", "format=duration", "-v", "quiet", "-of", "csv=p=0"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout)
+
+
+
+# def separeteAudio(video):
+#     subprocess.run(['ffmpeg', '-i', video, '-vn',
+#                    '-acodec', 'mp3', video[:-4] + ".mp3"])
--- Backend/server.py
+++ Backend/server.py
@@ -8,6 +8,7 @@ import os
 from Mongo_cli import MongoCli
 from bson.objectid import ObjectId
 from datetime import datetime
+from data_characteristics import *
 # from features.mediapipe_handgesture.mediapipe_handgesture import Mediapipe_handgesture
 
 app = Flask(__name__)
@@ -18,16 +19,17 @@ CORS(app)
 
 
 class Operations:
-    def __init__(self): 
+    def __init__(self):
         pass
 
     def new_project(self):
         description = json.loads(request.form['description'])
         _id = mongo_cli.generate_unique_id()
-        data = {"name":description['name'], "subject": description['subject'], "model": description['model'], "category": description['category'], "content": [], "_id": str(_id), "update": datetime.now(), "privacy": 0, "features": []}
-        mongo_cli.insert_data(data,_id,"info")
+        data = {"name": description['name'], "subject": description['subject'], "model": description['model'],
+                "category": description['category'], "content": [], "_id": str(_id), "update": datetime.now(), "privacy": 0, "features": []}
+        mongo_cli.insert_data(data, _id, "info")
         return {"result": str(_id)}
-    
+
     def delete_project(self, project_id):
         project = mongo_cli.find_project(project_id)
         if project:
@@ -36,25 +38,36 @@ class Operations:
             videos = []
             for video in project['content']:
                 videos.append(video['_id'])
-            mongo_cli.delete_videos(project_id,videos)
+            mongo_cli.delete_videos(project_id, videos)
             mongo_cli.delete_from_db(project_id)
             return {"result": "Project deleted successfully"}
         else:
             return {"result": "Project not found"}
 
     def upload(self):
-        file = request.files['file']    
+        file = request.files['file']
         description = json.loads(request.form['description'])
-        # if(self.check_existing_name(description['name'])):  
+        # if(self.check_existing_name(description['name'])):
         #         return {"result": "Error"}
         info = './' + description['name'] + '.webm'
         file.save(info)
+
+        contrast = get_contrast(info)
+        brightness = get_brightness(info)
+        sharpness = get_sharpness(info)
+        saturation = get_saturation(info)
+        hue = get_hue(info)
+
+        video_characteristics = {"contrast" : contrast, "brightness" : brightness, "sharpness" : sharpness, "saturation" : saturation, "hue" : hue}
+
         _id = mongo_cli.generate_unique_id()
         video_class = description.get('class')
         video_length = description.get('length')
-        data = {"name":description['name'], "video_class":video_class, "length": video_length, "_id":str(_id), "update": datetime.now(), "MediaPipeHand": 0, "Characteristics": {}}
+        data = {"name": description['name'], "video_class": video_class, "length": video_length, "_id": str(
+            _id), "update": datetime.now(), "MediaPipeHand": 0, "Characteristics": video_characteristics}
+            
         # mongo_cli.insert_data(data,_id,"info")
-        mongo_cli.insert_media_file(_id,info)
+        mongo_cli.insert_media_file(_id, info)
         os.remove(info)
         return mongo_cli.insert_in_project(description['id'], data)
         # self.insert_into_project(str(_id),description['project_id'])
@@ -68,18 +81,20 @@ class Operations:
             feature = mongo_cli.find_feature(fid)
             for vid in feature['data']:
                 print(vid)
-                video = mongo_cli.find_video(vid['video_id'],pid)
+                video = mongo_cli.find_video(vid['video_id'], pid)
                 if video[feature['class']] == 0:
-                    d = {'id': pid, 'edit': feature['class'], 'video_id': vid['video_id'], 'new_elem': fid}
+                    d = {
+                        'id': pid, 'edit': feature['class'], 'video_id': vid['video_id'], 'new_elem': fid}
                     mongo_cli.edit(d)
         return "done"
 
     def upload_video(self, name, _id, pid, video_class, video_length, location):
-        data = {"name":name, "video_class":"video_class", "length": "video_length", "_id":str(_id), "update": datetime.now(), "MediaPipeHand": 0, "Characteristics": {}}
-        mongo_cli.insert_media_file(ObjectId(_id),location)
+        data = {"name": name, "video_class": "video_class", "length": "video_length", "_id": str(
+            _id), "update": datetime.now(), "MediaPipeHand": 0, "Characteristics": {}}
+        mongo_cli.insert_media_file(ObjectId(_id), location)
         os.remove(location)
         return mongo_cli.insert_in_project(pid, data)
-    
+
     def upload_folder(self):
         file = request.files['file']
         description = json.loads(request.form['description'])
@@ -96,19 +111,21 @@ class Operations:
             return self.upload_video(name, _id, pid, "description['video_class']", "description['video_length']", location)
         elif name == "features.json":
             print("FEATURES IMPORTED")
-            os.system("mongoimport --db=context_user --collection=features --file=features.json")
+            os.system(
+                "mongoimport --db=context_user --collection=features --file=features.json")
             for feature in mongo_cli.features.find():
                 if feature['info']['_id'] not in project['features']:
                     project['features'].append(str(feature['_id']))
-            mongo_cli.insert_data(project,pid,"info")
+            mongo_cli.insert_data(project, pid, "info")
             os.remove(location)
             return "DONE"
         elif name == "context_user.json":
             print("FILES IMPORTED")
-            os.system("mongoimport --db=context_user --collection=context_user --file=context_user.json")
+            os.system(
+                "mongoimport --db=context_user --collection=context_user --file=context_user.json")
             os.remove(location)
             return "DONE"
-        
+
     def export_project(self):
         description = json.loads(request.form['description'])
         pid = description['pid']
@@ -118,13 +135,14 @@ class Operations:
         for video in project['content']:
             mongo_cli.download_media_file(video['_id'])
 
-        os.system("mongoexport --collection=export --db=context_user --out=context_user.json")
-        os.system("mongoexport --collection=exportFeatures --db=context_user --out=features.json")
+        os.system(
+            "mongoexport --collection=export --db=context_user --out=context_user.json")
+        os.system(
+            "mongoexport --collection=exportFeatures --db=context_user --out=features.json")
         mongo_cli.exportFeatures.drop()
         mongo_cli.export.drop()
 
         return "DONE"
-
 
     def new_feature(self):
         description = json.loads(request.form['description'])
@@ -133,13 +151,14 @@ class Operations:
 
         if description['feature'] not in project['features']:
             project['features'].append(str(_id))
-            mongo_cli.insert_data(project,project['_id'],"info")
+            mongo_cli.insert_data(project, project['_id'], "info")
 
-        data = {"_id": str(_id), "name": description['name'], "class": description['feature'], "data": [], "update": datetime.now()}
-        mongo_cli.insert_feature(data,_id,"info")
-        
+        data = {"_id": str(_id), "name": description['name'], "class": description['feature'], "data": [
+        ], "update": datetime.now()}
+        mongo_cli.insert_feature(data, _id, "info")
+
         return "done"
-    
+
     def list_features(self):
         description = json.loads(request.form['description'])
         return mongo_cli.list_features(description['pid'])
@@ -172,21 +191,23 @@ class Operations:
         project = mongo_cli.find_project(description['pid'])
         video_id = description['video_id']
         fid = description['fid']
-        if fid not in project['features']: return None
+        if fid not in project['features']:
+            return None
         return mongo_cli.get_feature(fid, video_id)
-    
+
     def get_public_projects(self):
         projects = mongo_cli.list_project()
         ret = []
         # print(projects)
         i = 0
         for project in projects:
-            if i == 6: break
+            if i == 6:
+                break
             if project['privacy'] == 0:
                 ret.append(project)
             i += 1
         return ret
-    
+
     def download_features(self):
         description = json.loads(request.form['description'])
         project = mongo_cli.find_project(description['pid'])
@@ -197,21 +218,21 @@ class Operations:
                 f.write(str(feature['video_id']))
                 f.write("\n")
         return send_from_directory('./', fileName, as_attachment=True)
-    
+
     # def load_checked_videos(self):
     #     description = json.loads(request.form['description'])
     #     feature = mongo_cli.find_feature(description['id'])
     #     checks = []
-    #     for 
-    
+    #     for
+
     def load_info(self):
         description = json.loads(request.form['description'])
         project = mongo_cli.find_project(description['pid'])
         tags = []
         for video in project['content']:
-                tags.append(video['video_class'])
+            tags.append(video['video_class'])
         return {"name": project["name"], "description": project['subject'], "tags": tags, "category": project['category']}
-    
+
     # def edit_class(self):
     #     description = json.loads(request.form['description'])
     #     video_class = description.get('class')
@@ -219,7 +240,7 @@ class Operations:
     #     video_id = description.get('video')
     #     mongo_cli.edit_class(project_id,video_id,video_class)
     #     return "done"
-    
+
     # def edit_name(self):
     #     description = json.loads(request.form['description'])
     #     video_name = description.get('name')
@@ -227,26 +248,25 @@ class Operations:
     #     video_id = description.get('video')
     #     mongo_cli.edit_name(project_id,video_id,video_name)
     #     return "done"
-    
+
     # def edit_description(self):
     #     description = json.loads(request.form['description'])
     #     project_description = description.get('subject')
     #     project_id = description.get('id')
     #     mongo_cli.edit_description(project_id,project_description)
     #     return "done"
-    
+
     # def change_privacy(self):
     #     description = json.loads(request.form['description'])
     #     project_id = description.get('id')
     #     mongo_cli.change_privacy(project_id)
     #     return "done"
-    
+
     def edit_video(self):
         description = json.loads(request.form['description'])
         mongo_cli.edit(description)
         return "done"
 
-    
     # def insert_in_project(self, project_id, data):
 
     #     projects = mongo_cli.collection.find()
@@ -256,22 +276,23 @@ class Operations:
     #             print(project)
     #             return {"result": "Correct"}
     #     return {"result": "Error"}
-    
+
     def list_videos(self):
         description = json.loads(request.form['id'])
         project_id = description['id']
         return mongo_cli.list_video(project_id)
-    
+
     def list_projects(self):
         return mongo_cli.list_project()
-    
+
     def download(self):
         return mongo_cli.generate_from_db(ObjectId(request.form['_id']))
-    
+
     def delete_video(self):
         description = json.loads(request.form['_id'])
         # mongo_cli.delete_from_db(description['_id'])
-        mongo_cli.delete_videos(description['project_id'], description['video_id'])
+        mongo_cli.delete_videos(
+            description['project_id'], description['video_id'])
         return "done"
 
     # def check_existing_name(self,name):
@@ -279,110 +300,141 @@ class Operations:
     #         if(name == i['info']['name']):
     #             return True
     #     return False
-    
+
     # def insert_into_project(self,project_id,video_id):
     #     project = mongo_cli.collection.find_one({'_id':project_id})
     #     project['content'].append(video_id)
     #     print(project)
-    
-    
+
+    def video_characteristics(self):
+        description = json.loads(request.form['description'])
+        video_id = description['video_id']
+        project_id = description['project_id']
+
+        print(mongo_cli.video_characteristics(project_id, video_id))
+
+    def average_characteristics(self):
+        pass
+
+
 operation = Operations()
+
 
 @app.route('/')
 def default():
     return "Running..."
+
 
 @app.route('/new_project', methods=['POST'])
 def new_project():
     print("NEW PROJECT")
     return operation.new_project()
 
+
 @app.route('/delete_project/<project_id>', methods=['POST'])
 def delete_project(project_id):
     print("PROJECT DELETED")
     return operation.delete_project(project_id)
+
 
 @app.route('/upload', methods=['POST'])
 def upload():
     print("UPLOAD")
     return operation.upload()
 
+
 @app.route('/upload_folder', methods=['POST'])
 def upload_folder():
     print("UPLOAD FOLDER")
     return operation.upload_folder()
+
 
 @app.route('/export_project', methods=['POST'])
 def export_project():
     print("EXPORTING")
     return operation.export_project()
 
+
 @app.route('/update', methods=['POST'])
 def update():
     print("UPDATED")
     return operation.update()
+
 
 @app.route('/edit', methods=['POST'])
 def edit():
     print("EDIT")
     return operation.edit_video()
 
+
 @app.route('/download', methods=['POST'])
 def download():
     print("DOWNLOAD")
     return operation.download()
+
 
 @app.route('/load_info', methods=['POST'])
 def load_info():
     print("INFO")
     return operation.load_info()
 
+
 @app.route('/list_videos', methods=['POST'])
 def list_videos():
     print("LIST")
     return operation.list_videos()
+
 
 @app.route('/get_features', methods=['POST'])
 def get_features():
     print("GET FEATURES")
     return operation.get_features()
 
+
 @app.route('/get_public_projects')
 def get_public_projects():
     print("PUBLICS")
     return operation.get_public_projects()
+
 
 @app.route('/list_features', methods=['POST'])
 def list_features():
     print("LIST_FEATURES")
     return operation.list_features()
 
+
 @app.route('/list_projects')
 def list_projects():
     print("PROJECTS")
     return operation.list_projects()
+
 
 @app.route('/delete_video', methods=['POST'])
 def delete_video():
     print("DELETE")
     return operation.delete_video()
 
+
 @app.route('/new_feature', methods=['POST'])
 def new_feature():
     print("NEW_FEATURE")
     return operation.new_feature()
+
 
 @app.route('/extract_features', methods=['POST'])
 def extract_features():
     print("FEATURES")
     return operation.extract_features()
 
+
 @app.route('/download_features', methods=['POST'])
 def download_features():
     print("DOWNLOAD FEATURES")
     return operation.download_features()
 
-# audio 
+# audio
+
+
 @app.route('/upload_audio', methods=['POST'])
 def upload_audio():
     print("UPLOAD AUDIO")
@@ -391,6 +443,7 @@ def upload_audio():
     file.save('./'+description['name']+'.mp3')
     # a = fs.put(file)
     return {'status': 200}
+
 
 @app.route('/download_audio', methods=['POST'])
 def download_audio():
@@ -404,19 +457,20 @@ def download_audio():
         response.data = f.read()
     return response
 
+
 @app.route('/list_audio')
 def list_audio():
     print("LIST AUDIO")
     return [i for i in os.listdir('.') if i.endswith('.mp3')]
+
+@app.route('/video_characteristics')
+def video_characteristics():
+    print("VIDEO CHARACTERISTICS")
+    return operation.video_characteristics()
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
 
     # confirm that server is running
     # curl http://localhost:5001/
-    
-
-
-
-
-
