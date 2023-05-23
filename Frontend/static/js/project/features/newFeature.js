09bc7aaebef471bbb6b1728462058cd892d38996
Tiago Mostardinha <tiago.mostardinha@ua.pt>
2023-05-23 22:12:34 +0100

    preview das features outra vez no sitio

--- Frontend/static/js/project/features/newFeature.js
+++ Frontend/static/js/project/features/newFeature.js
@@ -518,11 +518,11 @@ function togglePreviewVideo(video_id,brightness,contrast,sharpness,saturation,hu
             newElem.className = "list-group";
             charsText.appendChild(newElem);
 
-            // const video = document.querySelector(newVideoID);
-            // video.setAttribute("id", "video" + video_id);
-            // tableLoadvideo(video_id)
-            // newVideoID = "#video" + video_id;
-            // visible = true;
+            const video = document.querySelector(newVideoID);
+            video.setAttribute("id", "video" + video_id);
+            tableLoadvideo(video_id)
+            newVideoID = "#video" + video_id;
+            visible = true;
 
             previewVideo.style.display = "block";
         } else{
